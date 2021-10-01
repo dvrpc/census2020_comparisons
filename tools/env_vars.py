422dcfaa2e21fdeca986e3679bf291906fb37336
aaronfraint <aaronfraint@icloud.com>
2021-10-01 12:03:45 -0400

    load dotenv

--- tools/env_vars.py
+++ tools/env_vars.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 
+load_dotenv(find_dotenv())
+
 DATABASE_URL = os.environ.get("DATABASE_URL")
 GDRIVE_PROJECT_ROOT = os.environ.get("GDRIVE_PROJECT_ROOT")
